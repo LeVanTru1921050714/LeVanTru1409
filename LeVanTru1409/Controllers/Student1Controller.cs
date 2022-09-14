fc23c153351d9da39182f8cbef1e97eb906cfc5a
LeVanTru1921050714 <lee27052001@gmail.com>
2022-09-14 23:00:56 +0700

    Commit_Note

---
+++ LeVanTru1409/Controllers/Student1Controller.cs
@@ -0,0 +1,32 @@
+﻿using LeVanTru1409.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeVanTru1409.Controllers
+{
+    public class Student1Controller : Controller
+    {
+        public IActionResult Index2()
+        {
+            // Khoi tao 1 list student
+            List<Student1> StdList = new List<Student1>()
+            {
+                new Student1 {StudentID2 = 1, StudentName2 = "Sinh Vien Tru", StudentAge2 = 18 },
+                new Student1 {StudentID2 = 2, StudentName2 = "Sinh Vien B", StudentAge2 = 17 },
+                new Student1 {StudentID2 = 3, StudentName2 = "Sinh Vien C", StudentAge2 = 21 },
+                new Student1 {StudentID2 = 4, StudentName2 = "Sinh Vien D", StudentAge2 = 18 },
+                new Student1 {StudentID2 = 5, StudentName2 = "Sinh Vien E", StudentAge2 = 22 },
+            };
+            ViewData["Student1"] = StdList;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Student1Controller std)
+        {
+            // string message = std.StudentID + "-";
+            // message += std.StudentName + "-";
+            // message += std.Age;
+            // ViewBag.TT = message;
+            return View();
+        }
+    }
+}
