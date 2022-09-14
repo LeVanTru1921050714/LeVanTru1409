40e0f11aef9d0fc33c3db8a19c3e8a33858681ef
LeVanTru1921050714 <lee27052001@gmail.com>
2022-09-14 23:12:25 +0700

    del

--- LeVanTru1409/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LeVanTru1409.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
